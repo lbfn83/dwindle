efc485cc85a9eb364befb84caf5b57e0a1044ff6
Mateo <mtaycruz@gmail.com>
2022-04-18 16:17:35 -0400

    added navbar layout to site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { JobPage } from './page/JobPage';
+import { Navbar } from './component/Navbar';
+
+import logo from './images/dwindleTMbigfinal.png'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='site-container'>
+      <div className='title-bar'>
+        <div className='logo-image-container'><img src={logo} /></div> 
+        <button>Post a Job</button>
+      </div>
+
+      <div className='page-container'>
+        <Navbar />
+        <JobPage />
+      </div>
+
     </div>
   );
 }
