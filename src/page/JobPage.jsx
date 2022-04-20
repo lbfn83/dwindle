5aa6efe59a61267122c7223ad91e8e960ed8ab59
Mateo <mtaycruz@gmail.com>
2022-04-20 17:12:32 -0400

    added first set of text to jobpage and adjusted some of the button styling

--- src/page/JobPage.jsx
+++ src/page/JobPage.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const JobPage = () => {
   return (
     <div>
-        
+        <div>
+          <h1 className='center'>
+            Find a job that helps you pay down student debt or lowers the cost of your education
+          </h1>
+        </div>
         
         
         
