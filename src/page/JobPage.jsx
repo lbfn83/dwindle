a28a07f1d12f24093f24241697519178ae8539fa
Mateo <mtaycruz@gmail.com>
2022-04-21 17:00:17 -0400

    added the job post search form

--- src/page/JobPage.jsx
+++ src/page/JobPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { EmailSignUp } from '../component/EmailSignUp'
+import { JobPostSearch } from '../component/JobPostSearch'
 
 export const JobPage = () => {
   return (
     <div className='job-page-body'>
         
       <EmailSignUp />
-
+      <JobPostSearch />
         
     </div>
   )
