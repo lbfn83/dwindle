cba40bae8e67ac654c5fa8e371b7134d40e511fb
Mateo <mtaycruz@gmail.com>
2022-05-04 16:30:11 -0400

    dropdown added

--- src/component/JobPostSearch.jsx
+++ src/component/JobPostSearch.jsx
@@ -45,8 +45,30 @@ export const JobPostSearch = () => {
 
         <form className='form-component' >
 
-            <input type="input" placeholder='keywords' onChange={handleChange}/>
-            <input type="input" placeholder='location' onChange={handleChange}/>
+            {/* <input type="input" placeholder='keywords' onChange={handleChange}/>
+            <input type="input" placeholder='location' onChange={handleChange}/> */}
+            <select name="pets" id="pet-select">
+                <option value="">--Please choose an option--</option>
+                <option value="dog" onChange={handleChange}>Customer Service Agent</option>
+                <option value="cat" onChange={handleChange}>Customer Liaison Officer</option>
+                <option value="hamster" onChange={handleChange}>Retail Associate</option>
+                <option value="parrot" onChange={handleChange}>Front Desk Manager</option>
+                <option value="spider" onChange={handleChange}>Server</option>
+                <option value="goldfish" onChange={handleChange}>Busser</option>
+            </select>
+
+            <select name="pets" id="pet-select">
+                <option value="">--Please choose an option--</option>
+                <option value="dog" onChange={handleChange}>Toronto</option>
+                <option value="cat"onChange={handleChange}>Virginia</option>
+                <option value="hamster" onChange={handleChange}>Vancouver</option>
+                <option value="parrot" onChange={handleChange}>Miami</option>
+                <option value="spider" onChange={handleChange}>Atlanta</option>
+                <option value="goldfish" onChange={handleChange}>Los Angeles</option>
+            </select>
+
+
+
             <button onClick={handleClick} disabled = {!(keywordField&&locationField)}>Find Jobs</button>
         </form>
 
