ed3e35ecfbf1cc7fdfe96a42314a1f28b7cefde4
lbfn83 <lbfn83@gmail.com>
2022-05-19 13:25:17 -0400

    testRouter.js /fetchJOBpostingData is finalized, which provides functionality to populate jobposting data pulled from API

--- server/routers/testRouter.js
+++ server/routers/testRouter.js
@@ -10,6 +10,58 @@ const db = require('../models')
 const company = db.company
 const jobposting = db.jobposting
 
+// https://stackoverflow.com/questions/18983138/callback-after-all-asynchronous-foreach-callbacks-are-completed
+router.get('/resWriteTest', async (req, res) => {
+    
+    function iterateArry(resolve, reject)
+    {
+        let counter = 0
+        const a = [1,2,3,4]
+        console.log('length : ' +a.length)
+        a.forEach(async (elem) => {
+            await setTimeout(()=> {
+                console.log('Hello world');
+                res.write('asdf', 'utf8')
+            }, 3000)
+            counter++;
+            if(counter === a.length)
+            {
+                console.log("done")
+                resolve("done")
+            }
+        })
+
+
+    }
+    
+    const promise = new Promise(iterateArry);
+    promise.then( ()=> {
+        console.log("end")
+        
+        res.end('ok')})
+    
+})
+
+router.get('/resWriteTest2', async (req, res) => {
+    
+    function asyncFunction (item, cb) {
+        setTimeout(() => {
+          console.log('done with', item);
+          res.write('asdf', 'utf8')
+          cb();
+        }, 3000);
+    }
+
+    let requests = [1,2,3,4 ].map((item) => {
+        return new Promise((resolve) => {
+          asyncFunction(item, resolve);
+        });
+    })
+    
+    Promise.all(requests).then(() => res.end('ok'));
+})
+
+
 router.get('/setupCompanyListFromTxt/:uuid', async (req, res) => {
     // how to use uuid and req.params 
     const uuid = req.params.uuid
@@ -48,131 +100,166 @@ router.get('/setupCompanyListFromTxt/:uuid', async (req, res) => {
 
 
 router.get('/fetchJOBpostingData', async (req, res) => {
-        var Logging = fs.createWriteStream('./QueryResult/logging.txt', {
-        flags: 'a' // 'a' means appending (old data will be preserved)
-      })
+        const dateLib  = new Date()
+        const dateStr = (dateLib.toDateString(Date.now())+" "+dateLib.getHours()).replace(/\s/g, '_')
+        fs.promises.mkdir('./Log', { recursive: true }).catch(console.error);
+        var Logging = fs.createWriteStream(`./Log/logging${dateStr}.txt`, {
+            flags: 'a' // 'a' means appending (old data will be preserved)
+        })
         const companyDBentries = await company.findAll()//.then((entries ) => {console.log("[Company DB entri]",entries)})
         const companyList = companyDBentries.map((element) => element.companyname )
-        const date  = new Date()
         const location = ['USA', 'CANADA']
-        Logging.write("<<<<<<<<"+ date.toDateString(Date.now())+" "+date.getHours() +">>>>>>>>>>")
+        Logging.write("<<<<<<<<"+ dateStr +">>>>>>>>>>")
         Logging.write("\n------Company List---------\n")
         
         companyList.forEach((each) => {
             Logging.write(each+'\n')
-    
         }) 
+        Logging.write("\n------------------------\n")
 
-        let idx = 0
-        let dataProcess = await companyList.forEach( async (companyName) => {
-            
-             await location.forEach( async (loc) => {
 
-                    var queryOption = {
-                        method: 'POST',
-                        url: 'https://linkedin-jobs-search.p.rapidapi.com/',
-                        headers: {
-                        'content-type': 'application/json',
-                        'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com',
-                        'X-RapidAPI-Key': `${process.env.API_KEY}`
-                        },  
-                    
-                        data: `{"search_terms":"${companyName}","location":"${loc}","page":"1","fetch_full_text": "yes"}`
-                    };  
-
-                    try
-                    {   
-                        const result = await axios.request(queryOption)
-                        
-                        Logging.write("\n------API response---------\n")
-                        Logging.write("\n---queryOption : " + queryOption.data + "----\n")
-
-                        idx += 1
-                        Logging.write("[res "+idx+" ] :" + companyName+"\n")
-                        Logging.write("[rowData] : " + JSON.stringify(result.data.length) + "[rowData End]\n")  
-
-                        if( result.data !== undefined && result.data.length > 0   )//|| result.data.length>0 )
-                        {
-                            result.data.forEach( async (element) => {
-                                Logging.write("[eachElem] : " + JSON.stringify(element) + "\n")
-
-                                // If API sends wrong data having different company name, simply ignore 
-                                // Chegg INC from API.. our database Chegg
-                                // Natixis assurance from API ... our database Natxis
-                                if(element.company_name.includes(companyName) )
-                                {
-                                    // TODO : see if same data entry already exists in database and 
-                                    // if so, doing updateValidation
-                                    element.normalized_job_location = loc
-                                    element.company_name = companyName
-                                    
-                                    /* The problem with below is that it doens't update the found data entry 
-                                    
-                                    Motivation : 
-                                    on daily basis, jobposting database should be updated with new or existing jobposting data pulled from API query
-                                    if existing data entry matches any of this new stream of data from API query, we simply 
-                                    update UpdatedAt column as current timestamp so it is not the target of soft-deletion*/
-                                    /*
-                                    const [elemFound, created] = await jobposting.findOrCreate({
-                                        where: { linkedin_job_url_cleaned: element.linkedin_job_url_cleaned },
-                                        defaults: element
-                                      });
-                                      // when created is false, it is just updated
-                                      Logging.write("Created? : "+ created + "//" + elemFound +"\n")
-                                    */
-                                    
-                                    const foundEntry = await jobposting.findOne({where : {
-                                        linkedin_job_url_cleaned: element.linkedin_job_url_cleaned 
-                                    }})
-                                    console.log("debugging is hard : " + foundEntry)
-                                    if(foundEntry !== null)
-                                    {
-                                        
-                                        // update
-                                       foundEntry.set(element)
-                                       Logging.write("[update] : " + JSON.stringify(foundEntry))
-                                       await foundEntry.save()
-                                       Logging.write("[/update]\n");
-                                    }else{
-                                        await jobposting.create(element)    
-                                        Logging.write("[insert]\n");    
-                                    }
-                                   
-                                }else
-                                {
-                                    // res.write
-                                    Logging.write("[error_from_API]"+element.normalized_company_name +" is not a search keyword\n")
-                                }
-                            })
-                        }else{
-
-                            Logging.write("[no data]\n")
-                        }   
-                            //   put validation here. don't allow null or soemthing differet from company NAme
-                            // also for better analysis, put log into the file
-                            //   jobposting.create(result.data[1]);
-                            //   jobposting.bulkCreate(result.data);
-                            // From Linked in to our backend server, we got object as a response, not string
-            
-                        }catch(error)
-                        {
-                            Logging.write("[error] : "+ error+ "\n")
-                        }
-                    })
-            
-            //   console.log("result Data in server: ", result)
-            //   await res.send(result.data)  
+        let combinedList = []
+        location.forEach( (loc) =>{
+            let result = companyList.map((comName) => {
+                    return {
+                        "company" : comName,
+                        "location" : loc 
+                    }
+            })
+            combinedList = [...combinedList, ...result]
         })
 
-       
-            // Logging.write("[end] ");
-            // Logging.end();
-        //TODO: why should this be asynchronous or otherwise it returns response way too fast 
-        async () => {res.send("good job")};  
-         
-        // return res.json(companyList)
-    //   res.status(400).send('Error while getting list of jobs.Try again later.');
-
+    
+        // TODO : await and async is really needed in here?
+        async function setupQueryOption(item, cb) {
+            var queryOption = {
+                method: 'POST',
+                url: 'https://linkedin-jobs-search.p.rapidapi.com/',
+                headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com',
+                'X-RapidAPI-Key': `${process.env.API_KEY}`
+                },  
+            
+                data: `{"search_terms":"${item.company}","location":"${item.location}","page":"1","fetch_full_text": "yes"}`
+            };  
+            await processAPIRequestAndSQL( queryOption , item.company, item.location, Logging).then((rtn) => {
+                console.log("rtn ;::", rtn)
+                // if(rtn !== undefined)
+                    res.write(JSON.stringify(rtn)+"/n")
+                cb();
+            })
+        }
+    
+        const promisePoolSearchKeywords = combinedList.map((item) => {
+            console.log("[item]",  item)
+            return new Promise((resolve) => {
+                setupQueryOption(item, resolve);
+            });
+        })
+     
+        Promise.all(promisePoolSearchKeywords).then(() => {
+            res.end("\n done \n")
+        });
+   
     })
+
+
+async function processAPIRequestAndSQL( queryOption , companyName, loc, Logging)
+{
+    try
+    {    
+      
+        const result = await axios.request(queryOption)
+        
+        Logging.write("\n------API response---------\n")
+        Logging.write("\n---queryOption : " + queryOption.data + "----\n")
+
+        Logging.write("[rowData] : " + JSON.stringify(result.data.length) + "[rowData End]\n")  
+
+        if( result.data !== undefined && result.data.length > 0   )//|| result.data.length>0 )
+        {
+            result.data.forEach( async (element) => {
+                Logging.write("[eachElem] : " + JSON.stringify(element) + "\n")
+
+                // If API sends wrong data having different company name, simply ignore 
+                // Chegg INC from API.. our database Chegg
+                // Natixis assurance from API ... our database Natxis
+                if(element.company_name.includes(companyName) )
+                {
+                    // TODO : see if same data entry already exists in database and 
+                    // if so, doing updateValidation
+                    element.normalized_job_location = loc
+                    element.company_name = companyName
+                    
+                    /* The problem with below is that it doens't update the found data entry 
+                    
+                    Motivation : 
+                    on daily basis, jobposting database should be updated with new or existing jobposting data pulled from API query
+                    if existing data entry matches any of this new stream of data from API query, we simply 
+                    update UpdatedAt column as current timestamp so it is not the target of soft-deletion*/
+                    /*
+                    const [elemFound, created] = await jobposting.findOrCreate({
+                        where: { linkedin_job_url_cleaned: element.linkedin_job_url_cleaned },
+                        defaults: element
+                      });
+                      // when created is false, it is just updated
+                      Logging.write("Created? : "+ created + "//" + elemFound +"\n")
+                    */
+                    
+                    /* Insert new jobposting item or Update existing DB entry corresponding to jobposting item*/
+                    const foundEntry = await jobposting.findOne({where : {
+                        linkedin_job_url_cleaned: element.linkedin_job_url_cleaned 
+                    }})
+                    console.log("[Select result]: " + foundEntry)
+                    if(foundEntry !== null)
+                    {
+                        
+                        // update
+                       foundEntry.set(element)
+                       Logging.write("[update] : " + JSON.stringify(foundEntry.linkedin_job_url_cleaned)+"\n")
+                       await foundEntry.save()
+                       
+                    }else{
+                        await jobposting.create(element)    
+                        Logging.write("[insert]"+JSON.stringify(element.linkedin_job_url_cleaned)+"\n");    
+                    }
+
+                }else
+                {
+                    // res.write
+                    Logging.write("[error_from_API]"+element.normalized_company_name +" is not a search keyword\n")
+                }
+            })
+            // In recursive manner, request next page from API end point
+
+            // data props in queryOption is not JSON object.. it is string that "looks like" JSON
+            let nextSearchOption = JSON.parse(result.config.data) 
+            nextSearchOption.page = (parseInt(nextSearchOption.page)+1).toString()
+            queryOption.data = JSON.stringify(nextSearchOption)
+
+            return await processAPIRequestAndSQL( queryOption , companyName, loc, Logging)
+          
+        }
+        else
+        {
+            Logging.write("[no data]\n")
+            return  {
+                "searched" : result.config.data
+            }
+        }   
+            //   put validation here. don't allow null or soemthing differet from company NAme
+            // also for better analysis, put log into the file
+            //   jobposting.create(result.data[1]);
+            //   jobposting.bulkCreate(result.data);
+            // From Linked in to our backend server, we got object as a response, not string
+
+    }catch(error)
+    {
+        
+        Logging.write("[error] : "+ error+ "\n")
+    }
+}
+
 
 module.exports = router
