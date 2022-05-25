c9a47a9e966645718c6e2b232882c8153c5fd53f
Mateo <mtaycruz@gmail.com>
2022-05-25 13:12:27 -0400

    added new Blog page to begin transfering over information

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom';
 import burgerMenu from '../images/icons8-menu.svg'
 
 export const Navbar = () => {
@@ -12,23 +12,23 @@ export const Navbar = () => {
         <div className="burger-menu" onClick={() => setOpen(!open)}><img src={burgerMenu} alt='' /></div>
 
         <ul className='nav-list'>
-            <li className="nav-option"><h3>About</h3></li>
-            <li className="nav-option"><a>Jobs</a></li>
-            <li className="nav-option"><a>Companies</a></li>
-            <li className="nav-option"><a>Benefits</a></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/community/'>Community</a></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/blog/'>Blog</a></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/partners/'>Partners</a></li>
+            <li className="nav-option"><Link to="/">About</Link></li>
+            <li className="nav-option"><Link to="/">Jobs</Link></li>
+            <li className="nav-option"><Link to="/">Companies</Link></li>
+            <li className="nav-option"><Link to="/">Benefits</Link></li>
+            <li className="nav-option"><Link to='/'>Community</Link></li>
+            <li className="nav-option"><Link to='/blog'>Blog</Link></li>
+            <li className="nav-option"><Link to='/'>Partners</Link></li>
         </ul>
                 
         <ul className='nav-mobile' style={{display: open ? 'block' : 'none'}}>
-            <li className="nav-option"><a href="#">About</a></li>
-            <li className="nav-option"><a href="#">Jobs</a></li>
-            <li className="nav-option"><a href="#">Companies</a></li>
-            <li className="nav-option"><a href="#">Benefits</a></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/community/'>Community</a></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/blog/'>Blog</a></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/partners/'>Partners</a></li>
+            <li className="nav-option"><Link to="/">About</Link></li>
+            <li className="nav-option"><Link to="/">Jobs</Link></li>
+            <li className="nav-option"><Link to="/">Companies</Link></li>
+            <li className="nav-option"><Link to="/">Benefits</Link></li>
+            <li className="nav-option"><Link to='/'>Community</Link></li>
+            <li className="nav-option"><Link to='/blog'>Blog</Link></li>
+            <li className="nav-option"><Link to='/'>Partners</Link></li>
         </ul>
       
 
---
+++ src/component/PostItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PostItems = () => {
+  return (
+    <div>
+        <a href="https://dwindlestudentdebt.com/blog/meet-the-expedia-of-student-loans/">
+            <h3>Meet The Expedia of Student Loans</h3>
+        </a> 
+    </div>
+  )
+}
