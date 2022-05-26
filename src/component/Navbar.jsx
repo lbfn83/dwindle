ce9c862a0508a853dc9c921a0c6518b0918c6ca0
Mateo <mtaycruz@gmail.com>
2022-05-26 11:40:24 -0400

    up dated nav links

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,21 +12,21 @@ export const Navbar = () => {
         <div className="burger-menu" onClick={() => setOpen(!open)}><img src={burgerMenu} alt='' /></div>
 
         <ul className='nav-list'>
-            <li className="nav-option"><Link to="/">About</Link></li>
+            <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
             <li className="nav-option"><Link to="/">Jobs</Link></li>
             <li className="nav-option"><Link to="/">Companies</Link></li>
             <li className="nav-option"><Link to="/">Benefits</Link></li>
-            <li className="nav-option"><Link to='/'>Community</Link></li>
+            <li className="nav-option"><a href='https://dwindlestudentdebt.com/community/'>Community</a></li>
             <li className="nav-option"><Link to='/blog'>Blog</Link></li>
-            <li className="nav-option"><Link to='/'>Partners</Link></li>
+            <li className="nav-option"><a href='/'>Partners</a></li>
         </ul>
                 
         <ul className='nav-mobile' style={{display: open ? 'block' : 'none'}}>
-            <li className="nav-option"><Link to="/">About</Link></li>
+            <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
             <li className="nav-option"><Link to="/">Jobs</Link></li>
             <li className="nav-option"><Link to="/">Companies</Link></li>
             <li className="nav-option"><Link to="/">Benefits</Link></li>
-            <li className="nav-option"><Link to='/'>Community</Link></li>
+            <li className="nav-option"><a href='https://dwindlestudentdebt.com/community/'>Community</a></li>
             <li className="nav-option"><Link to='/blog'>Blog</Link></li>
             <li className="nav-option"><Link to='/'>Partners</Link></li>
         </ul>
