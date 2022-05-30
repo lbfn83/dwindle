f69b34602af6ac0e705ded9911cae9e8ef7189c5
lbfn83 <lbfn83@gmail.com>
2022-05-30 13:38:34 -0400

    FrontEnd Search bar Changed with drop down menu

--- client/src/component/JobPostSearch.jsx
+++ client/src/component/JobPostSearch.jsx
@@ -1,6 +1,10 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './Search';
+import axios from 'axios'
+import { BACKEND_SVR_URL } from "../util/constants";
+// import AsyncSelect from 'react-select/async' 
+// import Select from 'react-select'
 
 // https://medium.com/geekculture/create-a-simple-search-component-in-react-js-using-react-hooks-710c1dfe8b58
 export const JobPostSearch = () => {
@@ -9,17 +13,46 @@ export const JobPostSearch = () => {
   const [locationField, setlocationField] = useState("")
   const [clickCounter, setClickCounter] = useState(0)
   
+  const [loading, setLoading] = useState(false)
+  const [arryCompany, setArryCompany] = useState([])
+
   const handleClick = (event) => {
     event.preventDefault()
     
     setClickCounter( clickCounter+1 )
   }
 
+  useEffect( () => {
+       
+    const apiReqString = `${BACKEND_SVR_URL}/database/company`
+ //    console.log(apiReqString)
+    setLoading(true)
+    axios.get(apiReqString).then(res => {
+          setLoading(false)
+          console.log("company list response : ",res.data)
+          setArryCompany(res.data)
+    })
+ }, [])
+
+
+  const colourOptions = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },
+  ];
   
+
   const handleChange = (event) => {
     // console.log(event)
     // console.log(event.target.placeholder)
-    switch(event.target.placeholder){
+    switch(event.target.id){
       case "location" :
         setlocationField(event.target.value)
         break;
@@ -33,6 +66,11 @@ export const JobPostSearch = () => {
     }
   }
 
+  const loadOptions = async(inputText, callback) => {
+    const response = ['abc', 'dec', 'eer', 'wqe'];
+    callback( response.map(i => ({label : i, value : i})))
+
+  }
 
   return (
     <div>
@@ -44,37 +82,43 @@ export const JobPostSearch = () => {
         <p>We only list jobs that provide student loan repayment, tuition assistance, and tuition reimbursement benefits</p>
 
         <form className='form-component' >
-
+{/* https://stackoverflow.com/questions/28868071/onchange-event-using-react-js-for-drop-down */}
+ 
             {/* <input type="input" placeholder='keywords' onChange={handleChange}/>
             <input type="input" placeholder='location' onChange={handleChange}/> */}
-            <select name="pets" id="pet-select">
+            <select id='keywords'  onChange={handleChange}>
                 <option value="">--Please choose an option--</option>
-                <option value="dog" onChange={handleChange}>Customer Service Agent</option>
-                <option value="cat" onChange={handleChange}>Customer Liaison Officer</option>
-                <option value="hamster" onChange={handleChange}>Retail Associate</option>
-                <option value="parrot" onChange={handleChange}>Front Desk Manager</option>
-                <option value="spider" onChange={handleChange}>Server</option>
-                <option value="goldfish" onChange={handleChange}>Busser</option>
+                {
+                  arryCompany.map( (p, index) =>
+                    <option value={p.companyname} >{p.companyname}</option>
+                  )
+                }
+  
+            </select>
+           
+            {/* <Select
+              
+                defaultValue={colourOptions[0]}
+                options={colourOptions}
+                    
+            /> */}
+           
+            <select id="location"  onChange={handleChange}>
+                <option value="">--Please choose an option--</option>
+                <option value="USA" >USA</option>
+                <option value="CANADA">CANADA</option>
             </select>
 
-            <select name="pets" id="pet-select">
-                <option value="">--Please choose an option--</option>
-                <option value="dog" onChange={handleChange}>Toronto</option>
-                <option value="cat"onChange={handleChange}>Virginia</option>
-                <option value="hamster" onChange={handleChange}>Vancouver</option>
-                <option value="parrot" onChange={handleChange}>Miami</option>
-                <option value="spider" onChange={handleChange}>Atlanta</option>
-                <option value="goldfish" onChange={handleChange}>Los Angeles</option>
-            </select>
 
 
-
-            <button onClick={handleClick} disabled = {!(keywordField&&locationField)}>Find Jobs</button>
+            <button onClick={handleClick} >Find Jobs</button> 
+            {/* disabled = {!(keywordField&&locationField)} */}
         </form>
 
         <form className = 'search-result'>
-            {/* <Search keyword = {keywordField} loc = {locationField} btnClicked = {clickCounter}/> */}
-            {(clickCounter>0)?<Search keyword = {keywordField} loc = {locationField} btnClicked = {clickCounter}/>: null}
+            <Search keyword = {keywordField} loc = {locationField} btnClicked = {clickCounter}/>
+            
+            {/* {(clickCounter>0)?<Search keyword = {keywordField} loc = {locationField} btnClicked = {clickCounter}/>: null} */}
         </form>
     
     </div>
--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -1,1 +1,2 @@
-export const BACKEND_SVR_URL = 'http://localhost:5000';
+export const BACKEND_SVR_URL = 'https://dwindle-backend.herokuapp.com'; 
+// 'http://localhost:5000'
