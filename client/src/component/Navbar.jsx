bf1ce6e1697562eb920e8a3c79c616791b6d6b85
Mateo <mtaycruz@gmail.com>
2022-06-01 12:06:40 -0400

    re added nav bar and links to new website

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import burgerMenu from '../images/icons8-menu.svg'
 
 export const Navbar = () => {
@@ -11,25 +11,25 @@ export const Navbar = () => {
     <div className="nav-container">  
         <div className="burger-menu" onClick={() => setOpen(!open)}><img src={burgerMenu} alt='' /></div>
 
-        {/* <ul className='nav-list'>
-            <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
+        <ul className='nav-list'>
+            <li className="nav-option"><a href="/">About</a></li>
             <li className="nav-option"><Link to="/">Jobs</Link></li>
             <li className="nav-option"><Link to="/">Companies</Link></li>
             <li className="nav-option"><Link to="/">Benefits</Link></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/community/'>Community</a></li>
+            <li className="nav-option"><a href='/'>Community</a></li>
             <li className="nav-option"><Link to='/blog'>Blog</Link></li>
             <li className="nav-option"><a href='/partners'>Partners</a></li>
         </ul>
                 
         <ul className='nav-mobile' style={{display: open ? 'block' : 'none'}}>
-            <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
+            <li className="nav-option"><a href="/">About</a></li>
             <li className="nav-option"><Link to="/">Jobs</Link></li>
             <li className="nav-option"><Link to="/">Companies</Link></li>
             <li className="nav-option"><Link to="/">Benefits</Link></li>
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/community/'>Community</a></li>
+            <li className="nav-option"><a href='/'>Community</a></li>
             <li className="nav-option"><Link to='/blog'>Blog</Link></li>
             <li className="nav-option"><Link to='/partners'>Partners</Link></li>
-        </ul> */}
+        </ul>
       
 
     </div>
