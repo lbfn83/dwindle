f447e1a6fc1bea0d96322ad47c90ba5522bd0f13
Mateo <mtaycruz@gmail.com>
2022-08-04 13:24:41 -0400

    fixed company card display

---
+++ client/src/component/Pagination.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pagination = () => {
+  return (
+    <div className="pagination">
+        <p>Previous</p>
+
+        <p>Next</p>
+
+    </div>
+  )
+}
+
+export default Pagination
