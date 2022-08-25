e7e1f04f7ddbde1356a81912c69397bc93a947d4
Mateo <mtaycruz@gmail.com>
2022-08-24 21:35:56 -0400

    updated burgermenu to display beside logo on right side

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import burgerMenu from '../images/icons8-menu.svg'
+import React from 'react'
+// import { Link } from 'react-router-dom';
+// import burgerMenu from '../images/icons8-menu.svg'
 
 export const Navbar = () => {
-  const [open, setOpen] = useState(false)
+  // const [open, setOpen] = useState(false)
 
 
   return (
 
     <div className="nav-container">  
-        <div className="burger-menu" onClick={() => setOpen(!open)}><img src={burgerMenu} alt='' /></div>
+        {/* <div className="burger-menu" onClick={() => setOpen(!open)}><img src={burgerMenu} alt='' /></div> */}
 
         <ul className='nav-list'>
             <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
             {/* <li className="nav-option"><Link to="/">Jobs</Link></li> */}
             <li className="nav-option"><a href="https://dwindlestudentdebt.com/companies/">Companies</a></li>
-            <li className="nav-option"><Link to="/benefit">Benefit</Link></li>
+            <li className="nav-option"><a href="https://dwindlestudentdebt.com/benefits/">Benefit</a></li>
             {/* <li className="nav-option"><a href='/'>Community</a></li> */}
             <li className="nav-option"><a href='https://dwindlestudentdebt.com/blog/'>Blog</a></li>
             <li className="nav-option"><a href='https://dwindlestudentdebt.com/partners/'>Partners</a></li>
         </ul>
                 
-        <ul className='nav-mobile' style={{display: open ? 'block' : 'none'}}>
-            <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
+        {/* <ul className='nav-mobile' style={{display: open ? 'block' : 'none'}}>
+            <li className="nav-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li> */}
             {/* <li className="nav-option"><Link to="/">Jobs</Link></li> */}
-            <li className="nav-option"><a href="https://dwindlestudentdebt.com/companies/">Companies</a></li>
-            <li className="nav-option"><Link to="/benefit">Benefit</Link></li>
+            {/* <li className="nav-option"><a href="https://dwindlestudentdebt.com/companies/">Companies</a></li>
+            <li className="nav-option"><Link to="/benefit">Benefit</Link></li> */}
             {/* <li className="nav-option"><a href='/'>Community</a></li> */}
-            <li className="nav-option"><a href='https://dwindlestudentdebt.com/blog/'>Blog</a></li>
+            {/* <li className="nav-option"><a href='https://dwindlestudentdebt.com/blog/'>Blog</a></li>
             <li className="nav-option"><a href='https://dwindlestudentdebt.com/partners/'>Partners</a></li>
             <button><a href='https://dwindlestudentdebt.com/add-company/'>Add a Company</a></button>
-        </ul>
+        </ul> */}
       
 
     </div>
