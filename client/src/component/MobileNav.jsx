f3ec28b2fcc3fdff167dd271f670ea5164428be0
Mateo <mtaycruz@gmail.com>
2022-08-27 13:45:35 -0400

    changed mobile links to connect to our pages

--- client/src/component/MobileNav.jsx
+++ client/src/component/MobileNav.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const MobileNav = (open) => {
 
   return (
     <ul className='nav-mobile' style={{display: open.open ? 'flex' : 'none'}}>
           {/* <ul className='nav-mobile' style={display}></ul> */}
-        <li className="nav-mobile-option"><a href="https://dwindlestudentdebt.com/about/">About</a></li>
+        <li className="nav-mobile-option"><Link to="/">Jobs</Link></li>
         {/* <li className="nav-option"><Link to="/">Jobs</Link></li> */}
-        <li className="nav-mobile-option"><a href="https://dwindlestudentdebt.com/companies/">Companies</a></li>
-        <li className="nav-mobile-option"><a href="https://dwindlestudentdebt.com/benefits/">Benefit</a></li>
+        <li className="nav-mobile-option"><Link to="/companies">Companies</Link></li>
+        <li className="nav-mobile-option"><Link to="/benefit">Benefits</Link></li>
         {/* <li className="nav-option"><a href='/'>Community</a></li> */}
-        <li className="nav-mobile-option"><a href='https://dwindlestudentdebt.com/blog/'>Blog</a></li>
-        <li className="nav-mobile-option"><a href='https://dwindlestudentdebt.com/partners/'>Partners</a></li>
-        <button><a href='#'>Add a Company</a></button>
+        <li className="nav-mobile-option"><Link to="/blog">Blog</Link></li>
+        <li className="nav-mobile-option"><Link to="/partners">Partners</Link></li>
+        <button>Add a Company</button>
     </ul>
   )
 }
