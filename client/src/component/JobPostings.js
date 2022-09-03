9f8b2a1daa60a41bc2da766f5078b31992dead62
lbfn83 <lbfn83@gmail.com>
2022-09-03 13:52:42 -0400

    [front end] jobpostings component : added image and redirect link

--- client/src/component/JobPostings.js
+++ client/src/component/JobPostings.js
@@ -5,7 +5,12 @@ export default function JobPostings({jobList}) {
   try{
     //   if use props as an argument, props.pokemon is the right way to refer to
       // console.log("jobitems from JobPostings function: ", jobList)
-      let temp = jobList.map(p => (<div> {p} </div>) )
+      const redirectLinkPrefix = "https://dwindlestudentdebt.com/company-";
+      const JLwithLink = jobList.map(p => {
+        p.redirectLink = redirectLinkPrefix+String(p.company_name).toLowerCase().replace(/\s/g,''); 
+        return p; 
+      })
+      // console.log(JLwithLink)
       // console.log("mapped array in JobItems", temp) 
       // const temp = Object.keys(joblist)
         // console.log("keys" , temp)
@@ -15,12 +20,18 @@ export default function JobPostings({jobList}) {
       
             return (
             <div>
-              { jobList.map( (p,index) =>(
+              { JLwithLink.map( (p,index) =>(
                  
                       [
                         <div key = {2*index-1} className="job-posting">
                           <div className='posting-text-container'>
-                            <h2>{p.company_name}</h2>
+                            {/* <h2>{p.company_name}</h2> */}
+                            {/* onClick={navigate(p.company_name)}  */}
+                              <a style={{display: 'inline', backgroundColor : '#FFFFFF', boxShadow : 'none'} } href={p.redirectLink} ><img src = {p.imagelink} width = "100" height = "100"></img></a> 
+                              
+                              <div style={{display: 'inline', verticalAlign :'top', marginLeft : '10px'}}>{p.company_name}</div>
+                              
+                            
                             <h1> {p.job_title} </h1> 
                             <div >{p.job_location} | {p.posted_date} </div>
                           </div>
