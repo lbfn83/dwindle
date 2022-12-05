7c3c0a216fd4779f0eefe571ef6009693791cdd8
lbfn83 <lbfn83@gmail.com>
2022-12-05 09:53:42 -0500

    Company Routes is being refined in progress with new test modules

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const companyRouter = require('./routes/companyRouter')
+const companyRouter = require('./routes/company')
 const subscribeRouter = require('./routes/subscribeRouter')
 const jobpostingRouter = require('./routes/jobPostingRouter')
 const googleOAuth2Router = require('./routes/googleOAuth2Router')
@@ -31,17 +31,22 @@ app.use(bp.json());
 app.use(morgan('combined', {stream : {
   write : (msg) => logger.info(msg.trim()),
 }}));
-/* !!!! use below to invoke sync() to newly create database and tables 
-* only use this in Iaas or Local server to initialize database !!!! 
-*/
-// initDatabase();
+/**express session prac  */
+// const session = require('express-session');
+// //  store: 'MemoryStore'
+// app.use(session({secret: 'my secret', resave: true, saveUninitialized: true}));
+// app.use((req, res, next) => {
+//   res.locals.test = req.session.test;
+//   // middleware SHOULD have this, man...
+//   console.log("locals" ,res.locals)
+//   next();
+// })
+// app.get('/', async(req, res, next) => {
+//   console.log("get get")
+//   res.locals.test = "Asdf";
+// });
 
-/* forcefully Drop and create Tables : use with caution! */
-// (async() => {
-//   await sequelize.sync({force : true}).then(()=>{
-//     console.log("sync done")
-//   })
-// })();
+
 
 
 
@@ -69,18 +74,20 @@ fs.readdirSync(routes_directory).forEach(route_file => {
 });
 */
 /* ********* Schedulers *********** */
-// const {registerCampaignCreateService} = require('./util/taskScheduler/campaignCreateScheduler');
-// const {registerCampaignSendService} = require('./util/taskScheduler/campaignSendScheduler');
-// const {registerCampaignUpdateService} = require('./util/taskScheduler/campaignUpdateScheduler');
-const {registerJPProcess} = require('./util/taskScheduler/jobpostingFetchScheduler');
-const {registerDBDumpScheduler} = require('./util/taskScheduler/dbDumpScheduler');
-const {registerGoogleTKpurgeScheduler} = require('./util/taskScheduler/googleTKpurgeScheduler');
-const {registerTemplateCreateService} = require('./util/taskScheduler/templateCreateScheduler');
 
-registerGoogleTKpurgeScheduler();
-registerJPProcess();
-registerDBDumpScheduler();
-registerTemplateCreateService();
+if(process.env.NODE_ENV !== 'development')
+{
+  const {registerJPProcess} = require('./util/taskScheduler/jobpostingFetchScheduler');
+  const {registerDBDumpScheduler} = require('./util/taskScheduler/dbDumpScheduler');
+  const {registerGoogleTKpurgeScheduler} = require('./util/taskScheduler/googleTKpurgeScheduler');
+  const {registerTemplateCreateService} = require('./util/taskScheduler/templateCreateScheduler');
+
+  registerGoogleTKpurgeScheduler();
+  registerJPProcess();
+  registerDBDumpScheduler();
+  registerTemplateCreateService();
+
+}
 
 /* **** google API OAuth2 ****** */
 const {initGoogleDrive} = require('./config/googleDrive');
@@ -123,55 +130,8 @@ app.listen(PORT, () => {
 });
 
 
-/*  Test for winston logger in production*/
-
-// setInterval(()=>{
-//   logger.info('TEST')
-//   logger.log('info', 'Hi, guys')
-// }, 10000)
 
 
-/* Below API End point is now obsolete as this application is not designed for open and exhaustive job search but only for
-pulling job postings from company on the list once in a day    */
-/*
-app.get('/jobs', async (req, res) => {
-    // console.log("req from client : ", req)
-  try {
-      let { search_terms = '',location = '', page = '1',fetch_full_text = 'yes' } = req.query;
-   
-    search_terms = search_terms ? encodeURIComponent(search_terms) : '';
-    location = location ? encodeURIComponent(location) : '';
-    
-
-
-    // TODO : something that should be used later when making multiple request altogether 
-    // axios.all([
-    // axios.get('https://api.github.com/users/mapbox'),
-    // axios.get('https://api.github.com/users/phantomjs')
-    // ])
-
-
-    const query = {
-        method: 'POST',
-        url: 'https://linkedin-jobs-search.p.rapidapi.com/',
-        headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com',
-          'X-RapidAPI-Key': `${process.env.API_KEY}`
-        },  
-        // TODO : should  replace 'single quote into null 
-        data: `{"search_terms":"${search_terms}","location":"${location}","page":"${page}","fetch_full_text": "${fetch_full_text}"}`
-      };  
-    // console.log("query!!!!!!   ", query)
-    const result = await axios.request(query)
-    // From Linked in to our backend server, we got object as a response, not string
-
-    // console.log("result Data in server: ", result)
-    res.send(result.data);
-
-  } catch (error) {
-    console.log("error :" , error)
-    res.status(400).send('Error while getting list of jobs.Try again later.');
-  }
-});
-*/
+// below is added for the test code
+// https://stackoverflow.com/questions/33986863/mocha-api-testing-getting-typeerror-app-address-is-not-a-function
+module.exports = app;
